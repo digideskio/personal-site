6db9269f843bdbffd96473edb17a56b8ac3e9af1
Jamin X. Chen <chenxm35@gmail.com>
2015-01-07 14:14:39 +0800

    a blog of sci journals

--- publish.sh
+++ publish.sh
@@ -1,1 +1,10 @@
-sudo pelican content -o output -s pelicanconf.py && ghp-import output && git push -f git@github.com:caesar0301/caesar0301.github.io.git gh-pages:master
+#!/bin/bash
+
+mkdir -p output
+sudo pelican content -o output -s pelicanconf.py
+
+ghp-import output
+
+git push \
+    -f git@github.com:caesar0301/caesar0301.github.io.git \
+    gh-pages:master
